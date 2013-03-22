4ec70545bf378867aa9f3c374d46c20f8df8abbf
Omnia Saad ElDawy <osmdawy@gmail.com>
2013-03-22 21:53:02 +0200

    Some string operations are not supported in C++.

---
+++ src/StringOperations.cpp
@@ -0,0 +1,30 @@
+/*
+ * StringOperations.cpp
+ *
+ *  Created on: Mar 22, 2013
+ *      Author: Saad Dawy
+ */
+
+#include "StringOperations.h"
+#include <strstream>
+#include <string>
+StringOperations::StringOperations() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StringOperations::~StringOperations() {
+	// TODO Auto-generated destructor stub
+}
+vector<string>StringOperations::split(string str){
+
+                    string buf; // Have a buffer string
+                    stringstream ss(str); // Insert the string into a stream
+
+                    vector<string> tokens; // Create vector to hold our words
+
+                    while (ss >> buf)
+                        tokens.push_back(buf);
+                    return tokens;
+}
+
---
+++ src/StringOperations.h
@@ -0,0 +1,23 @@
+/*
+ * StringOperations.h
+ *
+ *  Created on: Mar 22, 2013
+ *      Author: Saad Dawy
+ */
+
+#ifndef STRINGOPERATIONS_H_
+#define STRINGOPERATIONS_H_
+#include <vector>
+#include <string>
+#include "StringOperations.h"
+#include <sstream>
+using namespace std;
+class StringOperations {
+public:
+	StringOperations();
+	static vector<string> split(string str);
+	virtual ~StringOperations();
+
+};
+
+#endif /* STRINGOPERATIONS_H_ */
